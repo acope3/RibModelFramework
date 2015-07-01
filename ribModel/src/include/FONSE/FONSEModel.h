43f8c114683b7b239f97c4824fe83515ef301fbc
Jeremy Rogers <rogers.jeremyn@gmail.com>
2015-07-01 19:31:22 -0400

    Uploaded FONSE headers and initial makefile
    Makefile does not work yet!

---
+++ ribModel/src/include/FONSE/FONSEModel.h
@@ -0,0 +1,72 @@
+// FONSEModel.h
+
+#ifndef FONSEMODEL_H
+#define FONSEMODEL_H
+
+#include "../base/Model.h"
+#include "FONSEParameter.h"
+
+class FONSEModel : public Model
+{
+private:
+
+	FONSEParameter *parameter;
+	virtual void obtainCodonCount(SequenceSummary& seqsum, std::string curAA, int codonCount[]);
+	double calculateLogLikelihoodPerAAPerGene(unsigned numCodons, int codonCount[], double mutation[], double selection[], double phiValue);
+
+public:
+
+	FONSEModel();
+	virtual ~FONSEModel();
+	FONSEModel(const FONSEModel& other);
+
+	void setParameter(FONSEParameter &_parameter);
+	virtual void calculateCodonProbabilityVector(unsigned numCodons, double* mutation, double* selection, double phi, double* codonProb);
+	// Likelihood ratio functions
+	virtual void calculateLogLiklihoodRatioPerGene(Gene& gene, int geneIndex, unsigned k, double* logProbabilityRatio);
+	virtual void calculateLogLikelihoodRatioPerGroupingPerCategory(std::string grouping, Genome& genome, double& logAcceptanceRatioForAllMixtures);
+
+	//Parameter wrapper functions:
+	virtual void initTraces(unsigned samples, unsigned num_genes) { parameter->initAllTraces(samples, num_genes); }
+	virtual void writeRestartFile(std::string filename) { return parameter->writeEntireRestartFile(filename); }
+	virtual double getSphi(bool proposed = false) { return parameter->getSphi(proposed); }
+	virtual double getSphiProposalWidth() { return parameter->getSphiProposalWidth(); }
+	virtual unsigned getNumMixtureElements() { return parameter->getNumMixtureElements(); }
+	virtual double getCategoryProbability(unsigned i) { return parameter->getCategoryProbability(i); }
+	virtual void proposeCodonSpecificParameter() { parameter->proposeCodonSpecificParameter(); }
+	virtual void adaptCodonSpecificParameterProposalWidth(unsigned adaptiveWidth) { parameter->adaptCodonSpecificParameterProposalWidth(adaptiveWidth); }
+	virtual void updateCodonSpecificParameter(std::string grouping) { parameter->updateCodonSpecificParameter(grouping); }
+	virtual void updateCodonSpecificParameterTrace(unsigned sample, std::string grouping) { parameter->updateCodonSpecificParameterTrace(sample, grouping); }
+	virtual void proposeSPhi() { parameter->proposeSPhi(); }
+	virtual unsigned getMixtureAssignment(unsigned index) { return parameter->getMixtureAssignment(index); }
+	virtual unsigned getSynthesisRateCategory(unsigned mixture) { return parameter->getSynthesisRateCategory(mixture); }
+	virtual unsigned getSelectionCategory(unsigned mixture) { return parameter->getSelectionCategory(mixture); }
+	virtual unsigned getMutationCategory(unsigned mixture)  { return parameter->getMutationCategory(mixture); }
+	virtual double getSynthesisRate(unsigned index, unsigned mixture, bool proposed = false) { return parameter->getSynthesisRate(index, mixture, proposed); }
+	virtual double getCurrentSphiProposalWidth() { return parameter->getCurrentSphiProposalWidth(); }
+	virtual void updateSphi() { parameter->updateSphi(); }
+	virtual void updateSphiTrace(unsigned sample) { parameter->updateSphiTrace(sample); }
+	virtual void adaptSphiProposalWidth(unsigned adaptiveWidth) { parameter->adaptSphiProposalWidth(adaptiveWidth); }
+	virtual void proposeSynthesisRateLevels() { parameter->proposeSynthesisRateLevels(); }
+	virtual unsigned getNumSynthesisRateCategories() { return parameter->getNumSynthesisRateCategories(); }
+	virtual std::vector<unsigned> getMixtureElementsOfSelectionCategory(unsigned k) { return parameter->getMixtureElementsOfSelectionCategory(k); }
+	virtual void updateSynthesisRate(unsigned i, unsigned k) { parameter->updateSynthesisRate(i, k); }
+	virtual void setMixtureAssignment(unsigned i, unsigned catOfGene) { parameter->setMixtureAssignment(i, catOfGene); }
+	virtual void updateSynthesisRateTrace(unsigned sample, unsigned i) { parameter->updateSynthesisRateTrace(sample, i); }
+	virtual void updateMixtureAssignmentTrace(unsigned sample, unsigned i) { parameter->updateMixtureAssignmentTrace(sample, i); }
+	virtual void setCategoryProbability(unsigned mixture, double value) { parameter->setCategoryProbability(mixture, value); }
+	virtual void updateMixtureProbabilitiesTrace(unsigned sample) { parameter->updateMixtureProbabilitiesTrace(sample); }
+	virtual void adaptSynthesisRateProposalWidth(unsigned adaptiveWidth) { parameter->adaptSynthesisRateProposalWidth(adaptiveWidth); }
+	virtual void getParameterForCategory(unsigned category, unsigned param, std::string aa, bool proposal, double* returnValue)
+	{
+		parameter->getParameterForCategory(category, param, aa, proposal, returnValue);
+	}
+	virtual unsigned getGroupListSize() { return parameter->getGroupListSize(); } //TODO: make not hardcoded?
+	virtual std::string getGrouping(unsigned index) { return parameter->getGrouping(index); }
+	// R wrapper
+	std::vector<double> CalculateProbabilitiesForCodons(std::vector<double> mutation, std::vector<double> selection, double phi);
+
+protected:
+};
+
+#endif // FONSEMODEL_H
---
+++ ribModel/src/include/FONSE/FONSETrace.h
@@ -0,0 +1,43 @@
+#ifndef FONSETRACE_H
+#define FONSETRACE_H
+
+#include <iostream>
+#include <vector>
+#include <cctype>
+#include "../base/Trace.h"
+
+class FONSETrace : public Trace
+{
+private:
+	std::vector<std::vector<std::vector<double>>> mutationParameterTrace; //order: mutationcategoy, numparam, samples
+	std::vector<std::vector<std::vector<double>>> selectionParameterTrace; //order: selectioncategoy, numparam, samples
+
+public:
+	//Constructor & Destructors
+	FONSETrace();
+
+	//Init functions
+	void initAllTraces(unsigned samples, unsigned num_genes, unsigned numMutationCategories,
+		unsigned numSelectionCategories, unsigned numParam, unsigned numMixtures, std::vector<mixtureDefinition> &_categories);
+	void initFONSETraces(unsigned samples, unsigned numMutationCategories, unsigned numSelectionCategories, unsigned numParam);
+	void initMutationParameterTrace(unsigned samples, unsigned numMutationCategories, unsigned numParam);
+	void initSelectionParameterTrace(unsigned samples, unsigned numSelectionCategories, unsigned numParam);
+
+
+	//Getter functions
+	std::vector<double> getMutationParameterTraceByMixtureElementForCodon(unsigned mixtureElement, std::string& codon);
+	std::vector<double> getSelectionParameterTraceByMixtureElementForCodon(unsigned mixtureElement, std::string& codon);
+
+	unsigned getMutationCategory(unsigned mixtureElement) { return categories->at(mixtureElement).delM; }
+	unsigned getSelectionCategory(unsigned mixtureElement) { return categories->at(mixtureElement).delEta; }
+	//Update functions	
+	void updateCodonSpecificParameterTrace(unsigned sample, std::string aa, std::vector<std::vector<double>> &curMutParam, std::vector<std::vector<double>> &curSelectParam);
+
+	//R WRAPPER FUNCTIONS
+
+	//Getter functions
+	std::vector<double> getMutationParameterTraceByMixtureElementForCodonR(unsigned mixtureElement, std::string& codon); //R WRAPPER 
+	std::vector<double> getSelectionParameterTraceByMixtureElementForCodonR(unsigned mixtureElement, std::string& codon); //R WRAPPER
+};
+
+#endif //ROCTRACE_H
