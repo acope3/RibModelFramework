d60eb3859796e22c4afeb3a43dbac0965368ed10
clandere <clandere@users.noreply.github.com>
2015-07-22 15:42:22 -0400

    Delete Trace.d

--- ribModel/src/Default/Trace.d
+++
@@ -1,9 +0,0 @@
-Trace.d: ../Trace.cpp ../include/base/Trace.h \
- ../include/base/../SequenceSummary.h \
- ../include/base/../mixtureDefinition.h
-
-../include/base/Trace.h:
-
-../include/base/../SequenceSummary.h:
-
-../include/base/../mixtureDefinition.h:
