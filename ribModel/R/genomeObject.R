b2d619b67f019a4e09ba141f9453330176592260
Jeremy Rogers <rogers.jeremyn@gmail.com>
2015-12-15 13:56:41 -0500

    Beginning documentation of R functions

--- ribModel/R/genomeObject.R
+++ ribModel/R/genomeObject.R
@@ -1,4 +1,17 @@
-
+#' Genome initialization
+#' 
+#' \code{initializeGenomeObject} initializes the RCpp Genome object
+#' 
+#' @param file A string containing the location of the input file.
+#' @param fasta A boolean value which decides whether to initialize with a
+#'  fasta file or an RFP value file. (TRUE for fasta, FALSE for RFP)
+#' @param expression.file A string containing the location of a file containing
+#'  empirical expression rates, if needed.
+#' @param append A boolean value that states whether the genes should be appended
+#'  to the end of the genome
+#'
+#' @return This function has no return value.
+#' 
 initializeGenomeObject <- function(file, fasta=TRUE, expression.file=NULL, append=FALSE)
 {
   genome <- new(Genome)
