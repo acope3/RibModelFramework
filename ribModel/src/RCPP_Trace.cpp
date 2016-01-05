4f0801dfe389b073e0d022eceb3ec571b3577f3c
clandere <clandere@vols.utk.edu>
2016-01-05 02:34:06 -0500

    fixed RCPP_Trace.cpp

--- ribModel/src/RCPP_Trace.cpp
+++ ribModel/src/RCPP_Trace.cpp
@@ -1,9 +1,6 @@
 
 #ifndef STANDALONE
 #include "include/base/Trace.h"
-#include "include/ROC/ROCTrace.h"
-#include "include/RFP/RFPTrace.h"
-#include "include/FONSE/FONSETrace.h"
 #include <Rcpp.h>
 using namespace Rcpp;
 
@@ -11,12 +8,10 @@ RCPP_MODULE(Trace_mod)
 {
   class_<Trace>( "Trace" )
     //These methods have only a C++ implementation
-    //.method("getSphiTrace", &Trace::getSphiTrace)
-    .method("getSphiTraces", &Trace::getSphiTraces)
-    //.method("getAPhiTrace", &Trace::getAPhiTrace)
-    .method("getSphiAcceptanceRatioTrace", &Trace::getSphiAcceptanceRatioTrace)
-    .method("getCspAcceptanceRatioTraceForAA", &Trace::getCspAcceptanceRatioTraceForAA)
-    .method("getCspAcceptanceRatioTrace", &Trace::getCspAcceptanceRatioTrace)
+    .method("getStdDevSynthesisRateTraces", &Trace::getStdDevSynthesisRateTraces)
+    .method("getStdDevSynthesisRateAcceptanceRatioTrace", &Trace::getStdDevSynthesisRateAcceptanceRatioTrace)
+    .method("getCodonSpecficAcceptanceRatioTraceForAA", &Trace::getCodonSpecficAcceptanceRatioTraceForAA)
+    .method("getCodonSpecificAcceptanceRatioTrace", &Trace::getCodonSpecificAcceptanceRatioTrace)
 
 
     //These methods have specific R wrappers
@@ -29,57 +24,28 @@ RCPP_MODULE(Trace_mod)
     .method("getMixtureProbabilitiesTraceForMixture", &Trace::getMixtureProbabilitiesTraceForMixtureR)
     .method("getMixutreAssignmentTrace", &Trace::getMixtureAssignmentTrace)
     .method("getMixtureProbabilitiesTrace", &Trace::getMixtureProbabilitiesTrace)
-    .method("getExpectedPhiTrace", &Trace::getExpectedPhiTrace)
+    .method("getExpectedSynthesisRateTrace", &Trace::getExpectedSynthesisRateTrace)
     .method("getNumberOfMixtures", &Trace::getNumberOfMixtures)
     
-    .method("setSphiTraces", &Trace::setSphiTraces)
-    .method("setSphiAcceptanceRatioTrace", &Trace::setSphiAcceptanceRatioTrace)
+    .method("setStdDevSynthesisRateTraces", &Trace::setStdDevSynthesisRateTraces)
+    .method("setStdDevSynthesisRateAcceptanceRatioTrace", &Trace::setStdDevSynthesisRateAcceptanceRatioTrace)
     .method("setSynthesisRateTrace", &Trace::setSynthesisRateTrace)
     .method("setSynthesisRateAcceptanceRatioTrace", &Trace::setSynthesisRateAcceptanceRatioTrace)
     .method("setMixtureAssignmentTrace", &Trace::setMixtureAssignmentTrace)
     .method("setMixtureProbabilitiesTrace", &Trace::setMixtureProbabilitiesTrace)
-    .method("setCspAcceptanceRatioTrace", &Trace::setCspAcceptanceRatioTrace)
+    .method("setCodonSpecificAcceptanceRatioTrace", &Trace::setCodonSpecificAcceptanceRatioTrace)
 
+	.method("getSynthesisOffsetAcceptanceRatioTrace", &ROCTrace::getSynthesisOffsetAcceptanceRatioTrace)
+	.method("getSynthesisOffsetTrace", &ROCTrace::getAphiTraceR)
+	.method("getSynthesisOffsetAcceptanceRatioTraceForIndex", &ROCTrace::getSynthesisOffsetAcceptanceRatioTraceForIndex)
+	.method("getObservedSynthesisNoiseTrace", &ROCTrace::getObservedSynthesisNoiseTrace)
+	.method("setSynthesisOffsetTrace", &ROCTrace::setSynthesisOffsetTrace)
+	.method("setSynthesisOffsetAcceptanceRatioTrace", &ROCTrace::setSynthesisOffsetAcceptanceRatioTrace)
+	.method("setObservedSynthesisNoiseTrace", &ROCTrace::setObservedSynthesisNoiseTrace)
+	.method("setCodonSpecificParameterTrace", &ROCTrace::setCodonSpecificParameterTrace)
+	.method("getCodonSpecificParameterTrace", &ROCTrace::getCodonSpecificParameterTrace)
+	.method("getCodonSpecificParameterTraceByMixtureElementForCodon", &ROCTrace::getCodonSpecificParameterTraceByMixtureElementForCodon)
     ;
-
-  class_<ROCTrace>( "ROCTrace" )
-    .derives<Trace>("Trace")
-    //These methods have specific R wrappers
-    .method("getMutationParameterTraceByMixtureElementForCodon", &ROCTrace::getMutationParameterTraceByMixtureElementForCodonR)
-    .method("getSelectionParameterTraceByMixtureElementForCodon", &ROCTrace::getSelectionParameterTraceByMixtureElementForCodonR)
-    .method("getMutationParameterTrace", &ROCTrace::getMutationParameterTrace)
-    .method("getSelectionParameterTrace", &ROCTrace::getSelectionParameterTrace)
-    .method("getAphiAcceptanceRatioTrace", &ROCTrace::getAphiAcceptanceRatioTrace)
-	.method("getAphiTraces", &ROCTrace::getAphiTraceR)
-	.method("getAphiAcceptanceRatioTraceForIndex", &ROCTrace::getAphiAcceptanceRatioTraceForIndex)
-	.method("getSepsilonTraces", &ROCTrace::getSepsilonTraceR)
-	.method("setAphiTrace", &ROCTrace::setAphiTrace)
-    .method("setAphiAcceptanceRatioTrace", &ROCTrace::setAphiAcceptanceRatioTrace)
-	.method("setSepsilonTrace", &ROCTrace::setSepsilonTrace)
-	.method("setMutationParameterTrace", &ROCTrace::setMutationParameterTrace)
-	.method("setSelectionParameterTrace", &ROCTrace::setSelectionParameterTrace)
-
-    ;
-
-	class_<RFPTrace>("RFPTrace")
-		.derives<Trace>("Trace")
-		.method("getAlphaParameterTraceByMixtureElementForCodon", &RFPTrace::getAlphaParameterTraceByMixtureElementForCodonR)
-		.method("getAlphaParameterTrace", &RFPTrace::getAlphaParameterTrace)
-		.method("getLambdaPrimeParameterTraceByMixtureElementForCodon", &RFPTrace::getLambdaPrimeParameterTraceByMixtureElementForCodonR)
-		.method("getLambdaPrimeParameterTrace", &RFPTrace::getLambdaPrimeParameterTrace)
-        .method("setAlphaParameterTrace", &RFPTrace::setAlphaParameterTrace)
-        .method("setLambdaPrimeParameterTrace", &RFPTrace::setLambdaPrimeParameterTrace)
-		;
-
-	class_<FONSETrace>("FONSETrace")
-		.derives<Trace>("Trace")
-
-		.method("getMutationParameterTrace", &FONSETrace::getMutationParameterTrace)
-		.method("getSelectionParameterTrace", &FONSETrace::getSelectionParameterTrace)
-		//These methods have specific R wrappers
-		.method("getMutationParameterTraceByMixtureElementForCodon", &FONSETrace::getMutationParameterTraceByMixtureElementForCodonR)
-		.method("getSelectionParameterTraceByMixtureElementForCodon", &FONSETrace::getSelectionParameterTraceByMixtureElementForCodonR)
-		;
 }
 #endif
 
