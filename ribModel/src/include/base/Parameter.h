0b834bd51866fe81acc02210940a571f6d13ec17
clandere <clandere@vols.utk.edu>
2016-01-05 03:10:24 -0500

    forgot another semicolon... good job

--- ribModel/src/include/base/Parameter.h
+++ ribModel/src/include/base/Parameter.h
@@ -222,7 +222,7 @@ class Parameter {
 
 
 		//Other Functions:
-		SEXP calculateSelectionCoefficientsR(unsigned sample, unsigned mixture)
+		SEXP calculateSelectionCoefficientsR(unsigned sample, unsigned mixture);
 		std::vector<unsigned> getMixtureAssignmentR();
 		void setMixtureAssignmentR(std::vector<unsigned> _mixtureAssignment);
 		unsigned getMixtureAssignmentForGeneR(unsigned geneIndex);
