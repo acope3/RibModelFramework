df5794b315ba7fd66ff7932dfabe32dc6819d6e3
adixon4528 <adixon13@vols.utk.edu>
2016-06-29 11:58:06 -0400

    Added ability to alter zoom for log likelihood trace.
    Adds the functionality for the user to set the window for the zoomed in
    portion of the log likelihood trace. If the user keeps it at null, it
    will simply do the last tenth as it always has.

--- R/plotMCMCAlgorithmObject.R
+++ R/plotMCMCAlgorithmObject.R
@@ -8,14 +8,16 @@
 #' 
 #' @description This function will plot the logLikelihood trace, and if the Hmisc package is installed, it will 
 #'  plot a subplot of the logLikelihood trace with the first few samples removed.
-plot.Rcpp_MCMCAlgorithm <- function(x, ...)
+plot.Rcpp_MCMCAlgorithm <- function(x, zoom.window = NULL, ...)
 {
   loglik.trace <- x$getLogLikelihoodTrace()
   
   loglik.trace <- loglik.trace[-1]
   
   trace.length <- length(loglik.trace)
+  
   zoomStart <- round(0.9*trace.length)
+  zoomEnd <- trace.length
   logL <- mean(loglik.trace[zoomStart:trace.length])
   #TODO change main title
   plot(loglik.trace, type="l", main=paste("logPP:", logL), xlab="Sample", ylab="log(Posterior Probability)")
@@ -25,9 +27,13 @@ plot.Rcpp_MCMCAlgorithm <- function(x, ...)
   # TODO (Cedric): get rid of that line once problem with first element beeing 0 is solved
   loglik.trace <- loglik.trace[-1]
   
+  if(zoom.window != NULL) {
+    zoomStart <- round(zoom.window[1] * trace.length)
+    zoomEnd <- round(zoom.window[2] * trace.length)
+  }
   
   Hmisc::subplot(
-    plot(zoomStart:trace.length, loglik.trace[zoomStart:trace.length], type="l", xlab=NA, ylab=NA, las=2, cex.axis=0.55), 
+    plot(zoomStart:zoomEnd, loglik.trace[zoomStart:zoomEnd], type="l", xlab=NA, ylab=NA, las=2, cex.axis=0.55), 
     0.8*zoomStart, (min(loglik.trace, na.rm = T)+max(loglik.trace, na.rm = T))/2, size=c(3,2))
 }
 
