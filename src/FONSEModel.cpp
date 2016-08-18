f5b06dbf37884608078c7d175537f434e580048f
adixon4528 <adixon13@vols.utk.edu>
2016-08-18 12:45:15 -0400

    Fixed FONSE compilation error

--- src/FONSEModel.cpp
+++ src/FONSEModel.cpp
@@ -651,17 +651,6 @@ void FONSEModel::printHyperParameters()
 }
 
 
-/* getParameter (RCPP EXPOSED)
- * Arguments: None
- *
- * Returns the FONSEParameter of the model.
-*/
-FONSEParameter FONSEModel::getParameter()
-{
-	return *parameter;
-}
-
-
 void FONSEModel::setParameter(FONSEParameter &_parameter)
 {
 	parameter = &_parameter;
