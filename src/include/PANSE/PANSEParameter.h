b2a020a1d5341b0068a3d9de4169356490cb7bd8
Alexander Cope <alexandercope@Alexanders-MacBook-Air-5.local>
2025-07-09 17:35:51 -0400

    Adding code for ignoring NSEs when running with R interface

--- src/include/PANSE/PANSEParameter.h
+++ src/include/PANSE/PANSEParameter.h
@@ -136,9 +136,9 @@ class PANSEParameter: public Parameter {
 
 		//Constructors & Destructors:
 		PANSEParameter(std::vector<double> stdDevSynthesisRate, std::vector<unsigned> geneAssignment,
-			std::vector<unsigned> _matrix, unsigned _numElongationMixtures, bool splitSer = true, bool _estimateNSE);
+			std::vector<unsigned> _matrix, unsigned _numElongationMixtures, bool splitSer = true, bool _estimateNSE = true);
 		PANSEParameter(std::vector<double> stdDevSynthesisRate, unsigned _numMixtures, std::vector<unsigned> geneAssignment, unsigned _numElongationMixtures,
-			bool splitSer = true, std::string _mutationSelectionState = "allUnique", bool _estimateNSE);
+			bool splitSer = true, std::string _mutationSelectionState = "allUnique", bool _estimateNSE = true);
 
 
 
